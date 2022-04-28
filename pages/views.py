4026479bc96f7f8846efd37417527c4fa97caf06
derick-droid <derrickokinda9@gmail.com>
2022-04-28 21:15:52 +0300

    adding html dynamic link bar

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
+# to display a home page
 def home(request):
     return render(request, "pages/home.html")
-    
+# to display the about page
+def about(request):
+    return render(request, "pages/about.html")
+# to display serbvices page
+def services(request):
+    return render(request, "pages/services.html")
+
+#  to display contact page
+def contact(request):
+    return render(request, "pages/contact.html")
