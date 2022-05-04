72d815c92a5d3cd2749c8610310b6faf2499df51
derick-droid <derrickokinda9@gmail.com>
2022-05-04 16:04:38 +0300

    display team frontend

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Team
 
 # to display a home page
 def home(request):
-    return render(request, "pages/home.html")
+    # fetching data from data to the home view
+    teams = Team.objects.all() # this fetches all the data from Team model
+    data = {
+        'teams': teams,
+    }
+    
+    return render(request, "pages/home.html", data)
+
 # to display the about page
 def about(request):
-    return render(request, "pages/about.html")
+    teams = Team.objects.all()
+    data = {
+    'teams' : teams
+    }
+    return render(request, "pages/about.html", data)
+
 # to display serbvices page
 def services(request):
     return render(request, "pages/services.html")
