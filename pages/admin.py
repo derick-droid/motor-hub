04c4e3f92e8d73adfd6cdaea16237d52f89e18bd
derick-droid <derrickokinda9@gmail.com>
2022-05-05 09:12:15 +0300

    completed Team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 
-# Register your models here.
-admin.site.register(Team)
+# creating a class to display a variety of features in the admin
+
+class TeamAdmin(admin.ModelAdmin):
+    # function to allow display image in the admin panel
+    def thumbnail(self, object):
+        return format_html('<img src  = " {}" width = "50"/>'.format(object.photo.url))
+    
+    thumbnail.short_description = "images" # changing the title display to images
+    
+    list_display = ("id", "thumbnail" ,"first_name", "last_name", "designation","created_date","facebook_link","twitter_link")
+   
+    # creating links in the models
+    list_display_links = ("id", "thumbnail","first_name", "last_name", "designation","created_date","facebook_link","twitter_link")
+    
+    # creating search bar
+    search_fields = ("first_name", "last_name", "designation")
+    
+    # creating filter box
+    list_filter = ("designation",)
+    
+admin.site.register(Team, TeamAdmin)
