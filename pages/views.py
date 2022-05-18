366b121aa8930ae8e4d3b130bd73ea4705c61f3c
derick-droid <derrickokinda9@gmail.com>
2022-05-18 11:55:24 +0300

    improving search functionality to remove duplicate information

--- pages/views.py
+++ pages/views.py
@@ -9,12 +9,20 @@ def home(request):
     teams = Team.objects.all() # this fetches all the data from Team model
     featured_cars = Car.objects.order_by("-created_date").filter(is_featured = True)
     all_cars = Car.objects.order_by("-created_date")
-    search_fields = Car.objects.values("model", "year", "body_style")
+    # search_fields = Car.objects.values("model", "year", "body_style")
+    model_search = Car.objects.values_list("model", flat=True).distinct()
+    year_search = Car.objects.values_list("year", flat=True).distinct()
+    body_search = Car.objects.values_list("body_style", flat=True).distinct()
+    
     data = {
         'teams': teams,
         'featured_cars' : featured_cars,
         'all_cars': all_cars,
-        'search_fields' : search_fields,
+        # 'search_fields' : search_fields,
+        'model_search' : model_search,
+        'year_search' : year_search,
+        'body_search' : body_search,
+        
     }
     
     return render(request, "pages/home.html", data)
