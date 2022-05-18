88da8b09f1f57e6d205889531ab98377450b8cd9
derick-droid <derrickokinda9@gmail.com>
2022-05-18 12:25:09 +0300

    fixing search issues and query into database

--- cars/views.py
+++ cars/views.py
@@ -29,10 +29,37 @@ def search(request):
     cars = Car.objects.order_by("-created_date")
     
     # enable such functionality with keys
+    
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
             cars = cars.filter(description__icontains = keyword)
+            
+    # enable search functionality with model
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact = model)
+    
+    # enable search functionality with year        
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact = year)
+            
+    # enable search functionality with body style
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact = body_style)
+            
+    # enable search functionality with minimum and maximum prices
+    if 'min_price' in request.GET:
+        min_prie = request.GET['min_price']
+        max_price = request.GET['max_price']
+        
+        if max_price:
+           cars =  cars.filter(price__gte = min_prie, price__lte = max_price)
     
     data = {
         "cars" : cars,
