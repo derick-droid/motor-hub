f5dbde5442862525eb5bd21ede3a63b29f2c9caf
derick-droid <derrickokinda9@gmail.com>
2022-05-24 15:01:17 +0300

    logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -63,6 +63,10 @@ def register(request):
     
 
 def logout(request):
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, "You have logged out successfully")
+        return redirect('home')
     return redirect("home")
 
 def dashboard(request):
