6c48de79dbb4630f10e9db5aa496db5bf5bd915c
derick-droid <derrickokinda9@gmail.com>
2022-05-25 13:10:51 +0300

    inquiry admin customization

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models  import contacts
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
+    list_display_links =  ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
+    search_fields = ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
+    list_per_page: 25
+admin.site.register(contacts, ContactAdmin)
