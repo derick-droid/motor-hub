cdd9042f778e08383725be7f76a1773f9db2874c
derick-droid <derrickokinda9@gmail.com>
2022-05-31 22:04:29 +0300

    json data hosting

--- carzone/settings.py
+++ carzone/settings.py
@@ -34,7 +34,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # Application definition
 
 INSTALLED_APPS = [
-    'contacts.apps.ContactsConfig',
+    # 'contacts.apps.ContactsConfig',
     'accounts.apps.AccountsConfig',
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
--- contacts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models  import contacts
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
-    list_display_links =  ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
-    search_fields = ("id", "first_name", "last_name", "email", "car_title", "city", "create_date")
-    list_per_page: 25
-admin.site.register(contacts, ContactAdmin)
